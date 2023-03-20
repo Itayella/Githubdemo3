ae503ea568886794943ac8fe4f1105a01ec981aa
Itayella <itay.ella@gmail.com>
2023-03-20 10:03:06 +0200

    Initial Commit

---
+++ Githubdemo3/Githubdemo3App.swift
@@ -0,0 +1,17 @@
+//
+//  Githubdemo3App.swift
+//  Githubdemo3
+//
+//  Created by Itay Elia on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Githubdemo3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
